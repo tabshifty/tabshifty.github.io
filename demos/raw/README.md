57044d58a17ce7f58e90c15975ebacafa352adff
PC <cheetsui@163.com>
2022-01-09 00:35:32 +0800

    update

---
+++ demos/raw/README.md
@@ -0,0 +1,3 @@
+# example
+
+注意传输到`gitpages`时的路径问题
--- demos/raw/index.js
+++ demos/raw/index.js
@@ -2,6 +2,11 @@ window.onload = function () {
     var imgWidth 
     var imgHeight
     var imgFrame = document.querySelector('.img-frame')
+    var expand = document.querySelector('.expand-menu')
+    var menu = document.querySelector('div.menus')
+    var loader = document.querySelector('.loader')
+    var expandFlag = false
+    loader.parentNode.removeChild(loader)
     function setFrameHeight() {
         imgWidth = imgFrame.clientWidth
         imgHeight = imgWidth * (443/520) + 'px'
@@ -11,5 +16,27 @@ window.onload = function () {
     // alert(window.innerWidth)
     setFrameHeight()
     
-    window.onresize = setFrameHeight
+    window.onresize = function() {
+        setFrameHeight()
+        if (window.innerWidth > 1024 && expandFlag) {
+            menu.style.overflow ="hidden"
+            expandFlag = false
+        }
+    }
+    expand.addEventListener('click', function(evt) {
+        evt.stopPropagation()
+        if (expandFlag) {
+            menu.style.overflow ="hidden"
+        } else {
+            menu.style.overflow ="visible"
+        }
+        expandFlag = !expandFlag
+
+    })
+    menu.addEventListener('click', function() {
+        if (expandFlag) {
+            menu.style.overflow ="hidden"
+        }
+        expandFlag = !expandFlag
+    })
 }
